b354fb871acb2db82a7c33b3364a1a468c180769
sagarsubedi <sagarsubediofficial@gmail.com>
2021-05-01 01:12:20 -0400

    it's 1:12 AM. done for today

--- stringClass.cpp
+++ stringClass.cpp
@@ -31,5 +31,50 @@ int main(){
     cout << s4 << endl;
     cout << s5 << endl;
 
+    // check if empty string or not
+    if(s0.empty()) cout << "s0 is empty" << endl;
+
+    // append char to string
+    s0.append("I love cpp");
+    s0 += " and cpp is cool";
+    cout << s0 << endl;
+
+    // remove or erase content
+    cout << s0.length() << endl;
+    s0.clear();
+    cout << s0.length() << endl;
+
+    // compare two strings
+    s0 = "Apple";
+    s1 = "Mango";
+    // compares lexographically and returns -1, 0, or 1 just like in java
+    cout << s0.compare(s1) << endl;
+    if(s0 > s1) cout << "s0 is greater";
+    else cout << "s1 is greater" << endl;
+
+    // access ith char of a string
+    cout << s1[2] << endl;
+
+    // find substring
+    string s = "this is very fun i love it";
+    int index = s.find("very");
+    cout << "index of very is " << index << endl;
+
+    // remove a word from the string
+    string word = "very";
+    int len = word.length();
+    cout << s << endl;
+    s.erase(index, len+1); // start from the index and remove (length of the word) chars
+    cout << s << endl;
+    // at this point the word "very" will be removed
+
+
+    // iterate over string
+    for(int i=0; i<s.length(); i++) cout << s[i];
+
+    // iterators
+    
+
+
     return 0;
 }
