81a97f48d34e69ac4dbc7219f24f44433a532f0f
sagarsubedi <sagarsubediofficial@gmail.com>
2021-05-01 00:58:32 -0400

    quick updates

--- stringClass.cpp
+++ stringClass.cpp
@@ -6,14 +6,30 @@
 using namespace std;
 
 int main(){
+    cout <<"-----"<< endl;
 
     // we can init a string obj
-    // 
+    // initialized an object
     string s0;
+    // string constructor
     string s1("Hello");
+    // literal string assignment
     string s2 = "Hello World";
+    // copy constructor
     string s3(s2);
+    // assignment
+    string s4 = s3;
 
+    char a[] = {'a','b','c','d','\0'};
+    // create a string out of char array
+    string s5(a);
+
+    cout << s0 << endl;
+    cout << s1 << endl;
+    cout << s2 << endl;
+    cout << s3 << endl;
+    cout << s4 << endl;
+    cout << s5 << endl;
 
     return 0;
 }
